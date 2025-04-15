c6a087d510934716e576eb0a3058ed62bd9cdb39
Fabrizio Contu <fab.contu@gmail>
2025-04-15 13:26:14 +0200

    error page update

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -4,8 +4,13 @@ import { Link } from "react-router-dom";
 function Error404() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50 gap-12">
-      <h1 className="text-8xl font-bold text-lime-500 font-mono">Error 404</h1>
-
+      <p className="text-3xl font-semibold text-blue-500">404</p>
+      <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+        Page not found
+      </h1>
+      <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+        Sorry, we couldn’t find the page you’re looking for.
+      </p>
       <Link
         to="/"
         className=" min-w-8 rounded-md bg-blue-500 px-2 md:px-3 py-2 text-sm md:text-base font-medium text-white hover:bg-blue-600"
